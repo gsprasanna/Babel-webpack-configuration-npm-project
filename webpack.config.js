a76e162b9556f58ab17df9915b682d8cfba81c5a
gsprasanna <erprasanna97@gmail.com>
2019-08-26 23:07:24 +0530

    configured webpack and babel loader

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = options=> {
+    return {
+        entry: './index.js',
+        output: {
+          path: path.resolve(__dirname, 'lib'),
+          filename: 'bundle[hash].js'
+        }
+    ,module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
+    }
+};
